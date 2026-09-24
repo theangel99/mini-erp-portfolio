857c4322818e8b7347dc8321f80de74abfc60bd5
Rok Veber <rokveber9@gmail.com>
2026-09-24 22:42:54 +0200

    Fix 403 error: implement FilamentUser interface in User model
    - Add canAccessPanel() method required by Filament 5
    - All users can access admin panel (roles will be added in Phase 6)
    - Fixes 403 Forbidden error after login

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,7 +14,7 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -28,5 +30,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Determine if the user can access the Filament panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Za Fazo 1: vsi uporabniki imajo dostop
+        // V Fazi 6 bomo dodali vloge (admin, komercialist)
+        return true;
     }
 }

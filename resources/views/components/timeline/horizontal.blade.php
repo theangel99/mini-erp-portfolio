b050e97f01df8d3c38878b771da2fe8bf57b0d69
Rok Veber <rokveber9@gmail.com>
2026-09-25 14:01:58 +0200

    Fix timeline container height and alignment

--- resources/views/components/timeline/horizontal.blade.php
+++ resources/views/components/timeline/horizontal.blade.php
@@ -1,11 +1,11 @@
 @props(['milestones'])
 
-<div class="relative min-h-[200px] w-full px-8">
+<div class="relative w-full px-8 py-8" style="min-height: 300px;">
     {{-- Timeline line --}}
-    <div class="absolute top-8 left-8 right-8 h-0.5 bg-gray-200 dark:bg-gray-700"></div>
+    <div class="absolute top-8 left-8 right-8 h-0.5 bg-gray-200 dark:bg-gray-700" style="z-index: 0;"></div>
 
     {{-- Milestones --}}
-    <div class="relative flex justify-between items-start gap-2 pb-4">
+    <div class="relative flex justify-between gap-2" style="align-items: flex-start;">
         @foreach($milestones as $index => $milestone)
             <x-timeline.milestone-node
                 :milestone="$milestone"

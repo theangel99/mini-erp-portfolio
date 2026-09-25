cd298dac4f15697c78ea1003eb338b70d679ecfc
Rok Veber <rokveber9@gmail.com>
2026-09-25 11:24:12 +0200

    Fix UserFactory to use static Faker instance
    - Use Faker\Factory::create() directly instead of $this->faker
    - Prevents null reference errors when factory is called with overrides
    - Fixes seeding errors on production server

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => \Faker\Factory::create()->name(),
+            'email' => \Faker\Factory::create()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
